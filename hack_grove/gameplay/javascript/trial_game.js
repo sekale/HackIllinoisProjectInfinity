86bd68e3242612d27b4ecd1a8c0bdb549a9994ef
Abhishek Srikanth <srikanth@purdue.edu>
2016-02-20 14:26:29 -0500

    made selection menu for python to interact with

--- hack_grove/gameplay/javascript/trial_game.js
+++ hack_grove/gameplay/javascript/trial_game.js
@@ -6,7 +6,21 @@ canvas.height = 480;
 document.body.style.backgroundColor = "yellow";
 document.body.appendChild(canvas);
 
+var k = 30;
 
 ctx.fillRect(20,20,150,100);
-ctx.rect(40,40,250,200);
+ctx.rect(0,0,canvas.width,canvas.height);
 ctx.stroke();
+
+document.getElementById("lr_action").onchange = function()
+{
+    console.log("yoyo");
+    ctx.fillStyle = "rgba(0,0,0,1)";
+    ctx.fillText(document.getElementById("lr_action").value, 0,0);
+    console.log(document.getElementById("lr_action").value);
+    ctx.rect(k,90,350,200);
+    k += 10;
+    ctx.stroke();
+};
+
+console.log("hello world");
