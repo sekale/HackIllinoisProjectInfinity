968b2cb34bbefb69049833bc966825bba29f4eb6
Abhishek Srikanth <srikanth@purdue.edu>
2016-02-20 23:13:06 -0500

    draw roads done

--- hack_grove/gameplay/static/javascript/main_game.js
+++ hack_grove/gameplay/static/javascript/main_game.js
@@ -13,6 +13,7 @@ var canvas_padding = 10;
 var directionDriving = "CENTER";
 
 var players = [];
+var turnVal, turnDirection;
 
 var keyPlayerHeight;
 
@@ -31,36 +32,86 @@ function setup()
     document.body.style.backgroundColor = "black";
     document.body.appendChild(canvas);
 
-    keyPlayerHeight = canvas.height * 0.90;
+    keyPlayerHeight = canvas.height * 0.75;
 
     for(var i = 0; i < NO_OF_PLAYERS; i+=1)
     {
-        players.push( {x:(canvas.width * 0.25 + (canvas.width * i / 2) / NO_OF_PLAYERS), y:(keyPlayerHeight), speed:i+1 } );
+        players.push( {x:(canvas.width * 0.25 + (canvas.width * i / 2) / NO_OF_PLAYERS), y:(keyPlayerHeight), speed:i } );
     }
     for(var i = 1; i < NO_OF_PLAYERS; i+=1)
     {
-        players[i].y -= i * canvas.height * 0.10;
+        players[i].y -= i * canvas.height * 0.05;
     }
+
+    turnVal = 0;
+    turnDirection = 1;
+    console.log(turnVal, turnDirection);
 }
 
 function drawBG()
 {
-    ctx.fillStyle = "rgba(0,25,250,1)";
+    // Aagayam
+    ctx.fillStyle = "rgba(0,25,250,1)"; // neelam
     ctx.fillRect(0,0, canvas.width, canvas.height);
-    ctx.fillStyle = "rgba(0,255,10,1)";
+
+    // Tharai
+    ctx.fillStyle = "rgba(0,255,10,1)"; // pacchai
     ctx.fillRect(0,canvas.height*0.25, canvas.width, canvas.height);
+}
+
+function drawRoad()
+{
+    var turnVal_effective = (parseInt(turnVal/10)/100)
+    console.log(turnVal, turnDirection, turnVal_effective);
+
+    ctx.fillStyle = "rgba(255,0,0,0.7)";
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height);
+    ctx.lineTo(0, keyPlayerHeight * 1.15);
+        var avgx = ( canvas.width * (0.3 + turnVal_effective) + 0 ) / 2;
+        var avgy = ( canvas.height * 0.25 + keyPlayerHeight * 1.15) / 2;
+    ctx.quadraticCurveTo( avgx - canvas.width * (turnVal_effective), avgy, canvas.width * (0.3 + turnVal_effective), canvas.height * 0.25);
+    ctx.lineTo(canvas.width * (0.7 + turnVal_effective), canvas.height * 0.25);
+        avgx = ( canvas.width * (0.7 + turnVal_effective) + canvas.width ) / 2;
+        avgy = ( canvas.height * 0.25 + keyPlayerHeight * 1.15) / 2;
+    ctx.quadraticCurveTo(avgx - canvas.width * (turnVal_effective), avgy, canvas.width, keyPlayerHeight * 1.15);
+    ctx.lineTo(canvas.width, canvas.height);
+    ctx.closePath();
+    ctx.fill();
+
+    if (Math.abs(turnVal) == 110)
+    {
+        if (turnDirection == 1)
+        {
+            turnDirection = -1;
+        }
+        else
+        {
+            turnDirection = 1;
+        }
+    }
+    if (turnVal == 0)
+    {
+        turnDirection = parseInt(Math.random() * 10) % 2;
+        if(turnDirection == 0)
+        {
+            turnDirection = -1;
+        }
+    }
+
+    turnVal += turnDirection;
 }
 
 function drawPlayer()
 {
     ctx.fillStyle = "rgba(0,0,0,1)";
-    ctx.fillRect(players[0].x,keyPlayerHeight, 50, 50);
+    ctx.fillRect(players[0].x,keyPlayerHeight, 50, 150);
     for(var i = 1; i < NO_OF_PLAYERS; i+=1)
     {
-        if(players[i].y < players[0].y && players[i].y >= canvas.height * 0.20)
+        if(players[i].y < players[0].y && players[i].y >= canvas.height * 0.10)
         {
-            ctx.fillRect(players[i].x,players[i].y, 50, 50);
-            
+            ctx.fillRect(players[i].x,players[i].y, 50, 150);
+
             ctx.fillText(i, players[i].x,players[i].y);
         }
     }
@@ -101,6 +152,7 @@ function main()
 {
     // now = Date.now();
     drawBG();
+    drawRoad();
     drawPlayer();
     updateHorizontalDirections();
     updateVerticalDirections();
