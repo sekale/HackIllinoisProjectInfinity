444cbfd1cea2bce4608ad5102b56b87f9b2e6e44
pranavpunjabi@gmail.com <pranavpunjabi@gmail.com>
2016-02-21 02:38:06 -0500

    Added url for Sensor information

--- hack_grove/sensors/views.py
+++ hack_grove/sensors/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.template import Context, loader
+
+def turn(request):
+	return HttpResponse("This is index page 1")
+	
